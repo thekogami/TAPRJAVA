e45715084f5c3eb9c84a9906345225b3c17cde0e
kogami <felipemourao@univille.br>
2025-04-04 18:53:44 -0300

    recursividade, sequencia de fibonatti usando Memoization

--- lista/src/main/java/tapr/univille/com/Recursividade.java
+++ lista/src/main/java/tapr/univille/com/Recursividade.java
@@ -6,32 +6,42 @@ import java.io.File;
 // crie uma função recursiva
 
 public class Recursividade {
-    public static void main(String[] args) {
-        String diretorio = "/Users/felipemourao/Desktop/Developer/TAPRJAVA/lista/src/main/java/tapr/univille/com";
-        listarArquivos(diretorio);
+  public static void main(String[] args) {
+    String diretorio = "/Users/felipemourao/Desktop/Developer/TAPRJAVA/lista/src/main/java/tapr/univille/com";
+    listarArquivos(diretorio);
 
-        // listarArquivosRecursivo(diretorio);
-    }
+    listarArquivosRecursivo(diretorio);
+  }
 
-    public static void listarArquivos(String diretorio) {
-        File dir = new File(diretorio);
-        if (dir.isDirectory()) {
-            String[] arquivos = dir.list();
-            if (arquivos != null) {
-                for (String arquivo : arquivos) {
-                    System.out.println(arquivo);
-                }
-            }
-        } else {
-            System.out.println("O caminho fornecido não é um diretório.");
+  public static void listarArquivos(String diretorio) {
+    File dir = new File(diretorio);
+    if (dir.isDirectory()) {
+      String[] arquivos = dir.list();
+      if (arquivos != null) {
+        for (String arquivo : arquivos) {
+          System.out.println(arquivo);
         }
+      }
+    } else {
+      System.out.println("O caminho fornecido não é um diretório.");
     }
+  }
 
-    //  public static void listarArquivosRecursivo(String diretorio) {
-    //    File dir = new File(diretorio);
-    //    if (dir.isDirectory()) {
-    //      String[] arquivos = dir.list();
-    //      if (arquivos != null)
-    //    }
-    //  }
+  public static void listarArquivosRecursivo(String diretorio) {
+    File dir = new File(diretorio);
+    if (dir.isDirectory()) {
+      String[] arquivos = dir.list();
+      if (arquivos != null) {
+        for (String arquivo : arquivos) {
+          File file = new File(diretorio + "/" + arquivo);
+          System.out.println(file.getAbsolutePath());
+          if (file.isDirectory()) {
+            listarArquivosRecursivo(file.getAbsolutePath());
+          }
+        }
+      }
+    } else {
+      System.out.println("O caminho fornecido não é um diretório.");
+    }
+  }
 }
--- lista/src/main/java/tapr/univille/com/RecursividadeFibonacci.java
+++ lista/src/main/java/tapr/univille/com/RecursividadeFibonacci.java
@@ -1,22 +1,32 @@
 package tapr.univille.com;
 
 // Faça uma funçao recursiva que imprima o enésimo valor da sequencia de fibonatti
+// com Memoization
 
 public class RecursividadeFibonacci {
 
   private static int cont;
+  private static int[] memo;
 
   public static void main(String[] args) {
-    System.out.println(fibo(50));
+    int n = 50;
+    memo = new int[n + 1]; // Array para armazenar os valores já calculados
+    for (int i = 0; i <= n; i++) {
+      memo[i] = -1; // Inicializa o array com -1 para indicar valores não calculados
+    }
+    System.out.println(fibo(n));
     System.out.println(cont);
   }
-  
+
   public static int fibo(int n) {
     cont++;
     if (n <= 1) {
       return n;
     }
-    return fibo(n - 1) + fibo(n - 2);
+    if (memo[n] != -1) { // Verifica se o valor já foi calculado
+      return memo[n];
+    }
+    memo[n] = fibo(n - 1) + fibo(n - 2); // Calcula e armazena o valor no array
+    return memo[n];
   }
-
 }
